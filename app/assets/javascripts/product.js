3eabe5bb5d7f9d1b3ea634ba0f130a3b8ea58a12
Viktoras Truchanovicius <viktoras@bakedandbranded.com>
2015-04-12 11:40:04 -0400

    empty before append

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -7,7 +7,7 @@ function get_products(data, callback) {
       console.log(data);
       var products = HandlebarsTemplates['products/product']({products: data});
       console.log(products);
-      $('.products').append(products);
+      $('.products').empty().append(products);
     }
   });
 }
@@ -16,7 +16,7 @@ function update_products(callback) {
   var lumens = $("#luminosity").slider("value");
   var temperature = $("#kalvin").slider("value");
   var sq_footage = 100;
-  var sort_by = "price";
+  var sort_by = "lifetime";
   data = {luminosity: lumens, temperature: temperature, sq_footage: sq_footage, sort_by: sort_by};
   get_products(data);
 }

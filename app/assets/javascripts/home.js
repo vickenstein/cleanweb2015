66eb294f33adde8afc9916ff984fd2a210da1825
Victor Zeng <victor.zeng@styluxinc.com>
2015-04-11 22:57:32 -0400

    colored the shit out of that

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -12,6 +12,7 @@ $(function() {
 
   perspective = new THREE.Object3D();
   perspective.rotation.y = - Math.PI / 2;
+  perspective.rotation.x = Math.PI / 6;
 
   perspective.position.y = -20;
   perspective.position.z = -50;
@@ -24,43 +25,49 @@ $(function() {
   // star_light.lookAt(scene.position);
   // star_light.shadowCameraVisible = true;
   // scene.add(star_light);
-  scene.add( new THREE.AmbientLight( 0x222222 ) );
+  ambient_light = new THREE.AmbientLight( 0x222222 );
+  scene.add(ambient_light);
 
   sphere = new THREE.SphereGeometry( 0.5, 16, 8 );
   sphere_mesh = new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xff0040 } ) ) 
 
-  light1 = new THREE.PointLight( 0xffeecc, 0.5, 100 );
-  light1.position.set(-35, 52, 27);
+  light1 = new THREE.PointLight( 0xfff6ea, 0.5, 100 );
+  light1.position.set(-35, 50, 27);
   perspective.add(light1);
 
 
-  light2 = new THREE.PointLight( 0xffeecc, 0.5, 100 );
-  light2.position.set(-35, 52, 4.25);
+  light2 = new THREE.PointLight( 0xfff6ea, 0.5, 100 );
+  light2.position.set(-35, 50, 4.25);
   perspective.add(light2);
 
-  light3 = new THREE.PointLight( 0xffeecc, 0.5, 100 );
-  light3.position.set(-35, 52, -16.5);
+  light3 = new THREE.PointLight( 0xfff6ea, 0.5, 100 );
+  light3.position.set(-35, 50, -16.5);
   perspective.add(light3);
 
-  light4 = new THREE.PointLight( 0xffeecc, 0.5, 100 );
-  light4.position.set(-27, 52, -34);
+  light4 = new THREE.PointLight( 0xfff6ea, 0.5, 100 );
+  light4.position.set(-27, 50, -34);
   perspective.add(light4);
 
-  light5 = new THREE.PointLight( 0xffeecc, 0.5, 100 );
-  light5.position.set(1.5, 52, -34);
+  light5 = new THREE.PointLight( 0xfff6ea, 0.5, 100 );
+  light5.position.set(1.5, 50, -34);
   perspective.add(light5);
 
-  light6 = new THREE.PointLight( 0xffeecc, 0.5, 100 );
-  light6.position.set(30, 52, -34);
+  light6 = new THREE.PointLight( 0xfff6ea, 0.5, 100 );
+  light6.position.set(30, 50, -34);
   perspective.add(light5);
 
   loader = new THREE.JSONLoader();
   loader.load("assets/furniture.json", function(geometry) {
       geometry.computeFaceNormals();
       geometry.computeVertexNormals();
+      var wood = THREE.ImageUtils.loadTexture('assets/wood.jpeg');
+      wood.wrapS = THREE.RepeatWrapping;
+      wood.wrapT = THREE.RepeatWrapping;
+      wood.repeat.set( 3, 3 );
       var material = new THREE.MeshLambertMaterial({
         side: THREE.DoubleSide,
-        color: 0xFFFFFF
+        color: 0xfff9f0,
+        map: wood
       });
       var mesh = new THREE.Mesh(geometry, material);
       mesh.scale.set(10, 10, 10);
@@ -70,20 +77,29 @@ $(function() {
   loader.load("assets/room.json", function(geometry) {
       geometry.computeFaceNormals();
       geometry.computeVertexNormals();
+      var wall = THREE.ImageUtils.loadTexture('assets/wall.jpg');
+      wall.wrapS = THREE.RepeatWrapping;
+      wall.wrapT = THREE.RepeatWrapping;
+      wall.repeat.set( 25, 25 );
       var material = new THREE.MeshLambertMaterial({
         side: THREE.DoubleSide,
-        color: 0xFFFFFF
+        color: 0xffffff,
+        map: wall
       });
+      material.needsUpdate = true;
       var mesh = new THREE.Mesh(geometry, material);
       mesh.scale.set(10, 10, 10);
+
       perspective.add(mesh);
   });
   loader.load("assets/pillow.json", function(geometry) {
       geometry.computeFaceNormals();
       geometry.computeVertexNormals();
-      var material = new THREE.MeshLambertMaterial({
+      var material = new THREE.MeshPhongMaterial({
         side: THREE.DoubleSide,
-        color: 0x267598
+        color: 0xffffff,
+        specular: 0xfc2626,
+        shininess: 4
       });
       var mesh = new THREE.Mesh(geometry, material);
       mesh.scale.set(10, 10, 10);
@@ -92,9 +108,16 @@ $(function() {
   loader.load("assets/couch.json", function(geometry) {
       geometry.computeFaceNormals();
       geometry.computeVertexNormals();
-      var material = new THREE.MeshLambertMaterial({
+      var leather = THREE.ImageUtils.loadTexture('assets/leather.jpg');
+      leather.wrapS = THREE.RepeatWrapping;
+      leather.wrapT = THREE.RepeatWrapping;
+      leather.repeat.set( 10, 10 );
+      var material = new THREE.MeshPhongMaterial({
         side: THREE.DoubleSide,
-        color: 0x267598
+        color: 0x000000,
+        specular: 0xffffff,
+        shininess: 20,
+        map: leather
       });
       var mesh = new THREE.Mesh(geometry, material);
       mesh.scale.set(10, 10, 10);
@@ -105,7 +128,7 @@ $(function() {
       geometry.computeVertexNormals();
       var material = new THREE.MeshLambertMaterial({
         side: THREE.DoubleSide,
-        color: 0x267598
+        color: 0x000000
       });
       var mesh = new THREE.Mesh(geometry, material);
       mesh.scale.set(10, 10, 10);
@@ -114,9 +137,15 @@ $(function() {
   loader.load("assets/floor.json", function(geometry) {
       geometry.computeFaceNormals();
       geometry.computeVertexNormals();
-      var material = new THREE.MeshLambertMaterial({
+      var floor = THREE.ImageUtils.loadTexture('assets/floor.jpg');
+      floor.wrapS = THREE.RepeatWrapping;
+      floor.wrapT = THREE.RepeatWrapping;
+      floor.repeat.set( 2, 2 );
+      var material = new THREE.MeshPhongMaterial({
         side: THREE.DoubleSide,
-        color: 0x267598
+        shininess: 5,
+        color: 0xFFFFFF,
+        map: floor
       });
       var mesh = new THREE.Mesh(geometry, material);
       mesh.scale.set(10, 10, 10);
@@ -127,8 +156,10 @@ $(function() {
       geometry.computeVertexNormals();
       var material = new THREE.MeshPhongMaterial({
         side: THREE.DoubleSide,
-        color: 0xEEEEEE,
+        color: 0xFFFFFF,
         specular: 0xFFFFFF,
+        transparent: true,
+        opacity: 0.5,
         shininess: 30
       });
       var mesh = new THREE.Mesh(geometry, material);
@@ -138,80 +169,69 @@ $(function() {
   loader.load("assets/window.json", function(geometry) {
       geometry.computeFaceNormals();
       geometry.computeVertexNormals();
-      var material = new THREE.MeshLambertMaterial({
+      var material = new THREE.MeshPhongMaterial({
         side: THREE.DoubleSide,
-        color: 0x267598
+        color: 0x000000,
+        specular: 0xFFFFFF,
+        shininess: 20
       });
       var mesh = new THREE.Mesh(geometry, material);
       mesh.scale.set(10, 10, 10);
       perspective.add(mesh);
   });
-  // loader.load("assets/geometry.Cube.009Geometry.9.json", function(geometry) {
-  //     geometry.computeFaceNormals();
-  //     geometry.computeVertexNormals();
-  //     var material = new THREE.MeshLambertMaterial({
-  //       side: THREE.DoubleSide,
-  //       color: 0x267598
-  //     });
-  //     var mesh = new THREE.Mesh(geometry, material);
-  //     mesh.scale.set(10, 10, 10);
-  //     perspective.add(mesh);
-  // });
-  // loader.load("assets/geometry.Cube.010Geometry.9.json", function(geometry) {
-  //     geometry.computeFaceNormals();
-  //     geometry.computeVertexNormals();
-  //     var material = new THREE.MeshLambertMaterial({
-  //       side: THREE.DoubleSide,
-  //       color: 0x267598
-  //     });
-  //     var mesh = new THREE.Mesh(geometry, material);
-  //     mesh.scale.set(10, 10, 10);
-  //     perspective.add(mesh);
-  // });
-  // loader.load("assets/geometry.Cube.011Geometry.9.json", function(geometry) {
-  //     geometry.computeFaceNormals();
-  //     geometry.computeVertexNormals();
-  //     var material = new THREE.MeshLambertMaterial({
-  //       side: THREE.DoubleSide,
-  //       color: 0x267598
-  //     });
-  //     var mesh = new THREE.Mesh(geometry, material);
-  //     mesh.scale.set(10, 10, 10);
-  //     perspective.add(mesh);
-  // });
-  // loader.load("assets/geometry.Cube.012Geometry.9.json", function(geometry) {
-  //     geometry.computeFaceNormals();
-  //     geometry.computeVertexNormals();
-  //     var material = new THREE.MeshLambertMaterial({
-  //       side: THREE.DoubleSide,
-  //       color: 0x267598
-  //     });
-  //     var mesh = new THREE.Mesh(geometry, material);
-  //     mesh.scale.set(10, 10, 10);
-  //     perspective.add(mesh);
-  // });
-  // loader.load("assets/geometry.Cube.013Geometry.9.json", function(geometry) {
-  //     geometry.computeFaceNormals();
-  //     geometry.computeVertexNormals();
-  //     var material = new THREE.MeshLambertMaterial({
-  //       side: THREE.DoubleSide,
-  //       color: 0x267598
-  //     });
-  //     var mesh = new THREE.Mesh(geometry, material);
-  //     mesh.scale.set(10, 10, 10);
-  //     perspective.add(mesh);
-  // });
-  // loader.load("assets/geometry.Cube.016Geometry.9.json", function(geometry) {
-  //     geometry.computeFaceNormals();
-  //     geometry.computeVertexNormals();
-  //     var material = new THREE.MeshLambertMaterial({
-  //       side: THREE.DoubleSide,
-  //       color: 0x267598
-  //     });
-  //     var mesh = new THREE.Mesh(geometry, material);
-  //     mesh.scale.set(10, 10, 10);
-  //     perspective.add(mesh);
-  // });
+  loader.load("assets/window_pane.json", function(geometry) {
+      geometry.computeFaceNormals();
+      geometry.computeVertexNormals();
+      var material = new THREE.MeshLambertMaterial({
+        side: THREE.DoubleSide,
+        color: 0xFFFFFF,
+        shininess: 20,
+        transparent: true,
+        opacity: 0
+      });
+      var mesh = new THREE.Mesh(geometry, material);
+      mesh.scale.set(10, 10, 10);
+      perspective.add(mesh);
+  });
+  loader.load("assets/outside.json", function(geometry) {
+      geometry.computeFaceNormals();
+      geometry.computeVertexNormals();
+      var outside = THREE.ImageUtils.loadTexture('assets/outside.jpg');
+      var material = new THREE.MeshLambertMaterial({
+        side: THREE.DoubleSide,
+        color: 0xFFFFFF,
+        map: outside
+      });
+      var mesh = new THREE.Mesh(geometry, material);
+      mesh.scale.set(10, 10, 10);
+      perspective.add(mesh);
+  });
+  loader.load("assets/tv.json", function(geometry) {
+      geometry.computeFaceNormals();
+      geometry.computeVertexNormals();
+      var material = new THREE.MeshLambertMaterial({
+        side: THREE.DoubleSide,
+        color: 0x000000,
+        specular: 0xFFFFFF,
+        shininess: 30
+      });
+      var mesh = new THREE.Mesh(geometry, material);
+      mesh.scale.set(10, 10, 10);
+      perspective.add(mesh);
+  });
+  loader.load("assets/screen.json", function(geometry) {
+      geometry.computeFaceNormals();
+      geometry.computeVertexNormals();
+      var logo = THREE.ImageUtils.loadTexture('assets/logo.png');
+      var material = new THREE.MeshLambertMaterial({
+        side: THREE.DoubleSide,
+        color: 0xFFFFFF,
+        map: logo
+      });
+      var mesh = new THREE.Mesh(geometry, material);
+      mesh.scale.set(10, 10, 10);
+      perspective.add(mesh);
+  });
 
   camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000000);
 
@@ -243,13 +263,13 @@ $(function() {
   max_x = Math.PI / 4;
 
   hammer.on('panmove', function(event) {
-    var change_y = (event.deltaX / ($(window).width() * 40)) * Math.PI;
-    if (perspective.rotation.y - change_y < min_y && perspective.rotation.y - change_y > max_y) {
-      perspective.rotation.y -= change_y;
+    var change_y = ((lastPanX - event.deltaX) / ($(window).width())) * Math.PI;
+    if (perspective.rotation.y + change_y < min_y && perspective.rotation.y + change_y > max_y) {
+      perspective.rotation.y += change_y;
     }
-    var change_x = (event.deltaY / ($(window).width() * 40)) * Math.PI;
-    if (perspective.rotation.x - change_x < max_x && perspective.rotation.x - change_x > min_x) {
-      perspective.rotation.x -= change_x;
+    var change_x = ((lastPanY - event.deltaY) / ($(window).width())) * Math.PI;
+    if (perspective.rotation.x + change_x < max_x && perspective.rotation.x + change_x > min_x) {
+      perspective.rotation.x += change_x;
     }
     lastPanX = event.deltaX;
     lastPanY = event.deltaY;
@@ -280,10 +300,9 @@ $(function() {
   }
 
   $("#kalvin").slider({
-    range: true,
     min: kalvin_min,
     max: kalvin_max,
-    values: (kalvin_min + kalvin_max) / 2,
+    value: (kalvin_min + kalvin_max) / 2,
     change: function(event, ui) {
       var average = ui.value;
       change_light_hue(average);
@@ -306,10 +325,9 @@ $(function() {
   }
 
   luminosity_slider = $("#luminosity").slider({
-    range: true,
     min: luminosity_min,
     max: luminosity_max,
-    values: (luminosity_min + luminosity_max) / 2,
+    value: (luminosity_min + luminosity_max) / 2,
     change: function(event, ui) {
       console.log(ui);
       var average = ui.value;
